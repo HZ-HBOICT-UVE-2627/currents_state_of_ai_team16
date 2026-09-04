d8b41ffd047eb1372e7b31a17ef87b97eaca692f
mikaelvos <os0017@hz.nl>
2026-09-04 11:21:35 +0200

    fix: use JavaScript Svelte config for CI build

--- code/svelte.config.js
+++ code/svelte.config.js
@@ -1,7 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import type { SvelteConfig } from '@sveltejs/vite-plugin-svelte';
 
 export default {
   preprocess: vitePreprocess(),
-} satisfies SvelteConfig;
-
+};
